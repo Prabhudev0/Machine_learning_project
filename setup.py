4f9499107ceb14c8bd58f7547da3f7c93b485e62
Prabhudev0 <prabhudevangadi1995@gmail.com>
2023-05-11 09:02:06 +0530

    added data validation

--- setup.py
+++ setup.py
@@ -13,7 +13,6 @@ REQUIREMENT_FILE_NAME="requirements.txt"
 
 HYPHEN_E_DOT = "-e ."
 
-
 def get_requirements_list() -> List[str]:
     """
     Description: This function is going to return list of requirement
